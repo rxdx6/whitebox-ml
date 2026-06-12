7f4551f44dce3303909c0c73927fcd44ef1c4114
Rudolph Smith <111125747+rxdx6@users.noreply.github.com>
2026-06-12 17:20:01 +0530

    Move logistic regression comments to header
    Relocate the explanatory comments for the logistic function and log-likelihood from logistic-regression.cpp into logistic-regression.hpp to centralize documentation and keep the implementation file cleaner. No functional code changes.

--- logistic-regression.cpp
+++ logistic-regression.cpp
@@ -1,11 +1,3 @@
-// f(x) = 1 / (1 + e^(-(wx + b)))
-// To optimise: Maximise: (Products of i = 1 to N)(f(x[i])^y[i] * (1 -
-// f(x[i]))^(1 - y[i]))
-//
-// It is more convenient to maximise the log likelihood instead of likelihood
-// due to the exponential nature of f(x). The log likelihood is given by:
-// Summation(y[i]ln(f(x[i])) + (1 - y[i])ln(1 - f(x[i])))
-
 #include "logistic-regression.hpp"
 #include <vector>
 
